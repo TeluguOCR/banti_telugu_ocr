Corrected the Posterior calculation.

classify_banti.py
@@ -13,14 +13,15 @@ from ngram.bantry import Bantry, process_line_bantires
 from ngram.post_process import post_process
 
 ############################################# Arguments
+default_ngram = "ngram/mega.123.pkl"
 
 if len(sys.argv) < 5:
     print("Usage:"
     "\n{0} neuralnet_params.pkl banti_output.box scaler_params.scl codes.lbl "
-    "[gram.tri.pkl='ngram/eemaata.tri.pkl']"
+    "[gram.tri.pkl='{1}']"
     "\n\te.g:- {0} cnn_softaux_gold.pkl sample_images/praasa.box "
     "glyph/scalings/relative48.scl glyph/labelings/alphacodes.lbl"
-    "".format(sys.argv[0]))
+    "".format(sys.argv[0], default_ngram))
     sys.exit()
 
 nnet_prms_file_name = sys.argv[1]
@@ -30,7 +31,7 @@ labelings_file_name = sys.argv[4]
 try:
     trigram_file = sys.argv[5]
 except IndexError:
-    trigram_file = "ngram/eemaata.tri.pkl"
+    trigram_file = default_ngram
 
 
 ############################################# Load Params
@@ -68,19 +69,19 @@ for nsamples, metas, data in gg():
         line, word, aux0, aux1 = meta
 
         if ntwk.takes_aux():
-            aux_data = [[(aux0/ht, aux1/ht), (aux0/ht, aux1/ht)]]
+            aux_data = np.array([[(aux0/ht, aux1/ht), (aux0/ht, aux1/ht)]], dtype='float32')
             logprobs_or_feats, preds = tester(img, aux_data)
         else:
             logprobs_or_feats, preds = tester(img)
 
-        output.append((line, word, preds[0], logprobs_or_feats[0]))
+        output.append((line, word, preds[0], logprobs_or_feats[0], aux0, aux1))
 
 
 ############################################# Helpers
 
 def get_best_n(logprobab, n=5):
     topn = np.argsort(logprobab)[:-1 - n:-1]
-    ret = '\n'
+    ret = ''
     for i in range(n):
         ret += '{} {:2.0f}\t'.format(index_to_char(topn[i]),
                                      100 * np.exp(logprobab[topn[i]]))
@@ -92,7 +93,7 @@ best_match = ''
 stats = '\n'
 linenum = -1
 wordnum = 0
-for line, word, pred, logprob in output:
+for line, word, pred, logprob, aux0, aux1 in output:
     if line > linenum:
         best_match += '\n'
         stats += '\n'
@@ -105,7 +106,7 @@ for line, word, pred, logprob in output:
         print(wordnum, end=' ')
 
     best_match += index_to_char(pred)
-    stats += get_best_n(logprob)
+    stats += '\n@({:3d}, {:3d}) '.format(aux0, aux1) + get_best_n(logprob)
 
 print()
 
@@ -122,21 +123,21 @@ with open(out_file_name, 'w', encoding='utf-8') as out_file:
 
 
 ####################################################### Try N-gram
-nclasses = output[-1][-1].size
+nclasses = output[-1][-3].size
 chars = [index_to_char(i) for i in range(nclasses)]
 
 path.priorer.set_trigram(trigram_file)
 
 curr_line = 0
 line_bantries = []
-decency = -np.log(nclasses)
+decency = -2*np.log(nclasses)
 
 out_file_name = banti_file_name.replace('.box', '.gram.txt')
 print('Writing ngrammed output to ', out_file_name)
 ngramout = open(out_file_name, 'w', encoding='utf-8')
 
 
-for line, word, preds, logprobs in output:
+for line, word, preds, logprobs, _, _ in output:
     if line < curr_line:
         raise (ValueError, "Line number can not go down {}->{}".format(
             curr_line, line))

classify_training_data.py
@@ -32,7 +32,6 @@ from iast_unicodes import get_index_to_char_converter
 ############################################## Helpers
 
 
-
 def read_json_bz2(path2data):
     import bz2, json
     bz2_fp = bz2.BZ2File(path2data, 'r')
@@ -84,10 +83,13 @@ if aux_data_file:
 else:
     aux = None
 
-n_trin = x_data.shape[0]
+n_samples = x_data.shape[0]
 batch_sz = net_prms['training_params']['BATCH_SZ']
-n_trin_bth = n_trin // batch_sz
+n_batches = n_samples // batch_sz
 n_classes = y_data.max()
+n_training_batches = int(n_batches *
+                         net_prms['training_params']['TRAIN_ON_FRACTION'])
+
 
 ############################################## Compile Test Function
 tester = ntwk.get_test_model(x, y, aux, preds_feats=True)
@@ -98,17 +100,19 @@ print('Classifying images...')
 counts = defaultdict(int)
 wrongs = defaultdict(int)
 errors = defaultdict(lambda: defaultdict(list))
+train_test_errs = [0, 0]
 
-for i in range(n_trin_bth):
-    sym_err_rate, aux_stat, logprobs, predictions = tester(i)
+for ibatch in range(n_batches):
+    sym_err_rate, aux_stat, logprobs, predictions = tester(ibatch)
 
     for j in range(batch_sz):
-        index = i * batch_sz + j
+        index = ibatch * batch_sz + j
         truth = y_data[index]
         logprob, guess = logprobs[j], predictions[j]
         counts[index_to_char(truth)] += 1
 
         if guess != truth:
+            train_test_errs[ibatch >= n_training_batches] += 1
             wrongs[index_to_char(truth)] += 1
             rank_of_truth = sum(logprob > logprob[truth])
             prob_of_truth = int(100 * np.exp(logprob[truth]))
@@ -116,22 +120,26 @@ for i in range(n_trin_bth):
             errors[index_to_char(truth)][index_to_char(guess)].append(
                 (index, rank_of_truth, prob_of_truth, prob_of_first))
 
-    if i % 20 == 0:
-        cum_err_rate = sum(wrongs.values())*100./sum(counts.values())
-        print('{} of {} batches. Error {:.2f}%'.format(
-            i + 1, n_trin_bth, cum_err_rate))
+    if ibatch % 20 == 0 or ibatch == n_batches-1:
+        print('{} of {} batches. Errors Train:{} Test:{}'.format(
+            ibatch + 1, n_batches, *train_test_errs))
         #if i == 100: break
 
+cum_err_rate = sum(wrongs.values())/sum(counts.values())
+train_test_errs[0] /= n_training_batches * batch_sz
+train_test_errs[1] /= (n_batches-n_training_batches) * batch_sz
+print("Error Rates Cum:{:.2%} Train:{:.2%} Test:{:.2%}".format(cum_err_rate, *train_test_errs))
 ####################### HTML strings
 
 head = '''<!DOCTYPE html>
 <html><head><meta charset="UTF-8"></head> 
-<body><h2>Errors for file {} using the parameters {}</h2>
-<h2>Cumulative error rate: {:.2f}</h2>
-<h2>Network</h2> <pre>{}</pre>
+<body><h2>Errors for file {0} using the parameters {1}</h2>
+<h2>Cumulative error rate: {2:.2%}</h2>
+</br>Training: {4[0]:.2%} Test: {4[1]:.2%}
+<h2>Network</h2> <pre>{3}</pre>
 </br>N) Character_Shown:(wrongs of tested = error%)
 </br> Character_Seen Image_Shown(rank, truth's prob% vs max's prob%)...
-'''.format(x_data_file, neural_net_file, cum_err_rate, ntwk)
+'''.format(x_data_file, neural_net_file, cum_err_rate, ntwk, train_test_errs)
 filler_main = '\n</br><p> {}) {} ({} of {} = {:.2f}%)</p>'
 filler_sub = '\n</br>{}'
 filler_img = '\n<img src="data:image/png;base64,{0}" title="{1} {2}"/> ' \
@@ -148,11 +156,11 @@ print('Compiling output html file ', out_file_name)
 out_file = open(out_file_name, 'w',)
 out_file.write(head)
 
-for i in range(n_classes):
+for ibatch in range(n_classes):
     # Write Summary
-    u = index_to_char(i)
+    u = index_to_char(ibatch)
     error_rate = 100.0 * wrongs[u] / counts[u] if counts[u] > 0 else 0.
-    out_file.write(filler_main.format(i, u, wrongs[u], counts[u], error_rate))
+    out_file.write(filler_main.format(ibatch, u, wrongs[u], counts[u], error_rate))
 
     # Write each bad classification
     for k, v in errors[u].items():

ngram/path.py
@@ -17,39 +17,49 @@ def defaultdict_counter():
 
 class _Prior():
     def __init__(self):
-        self.tricount = None
+        self.uni, self.bi, self.tri = None, None, None
 
     def set_trigram(self, pklfile):
         with open(pklfile, 'rb') as fp:
-            self.tricount = pickle.load(fp)
+            self.uni, self.bi, self.tri = pickle.load(fp)
 
-    def __call__(self, chars):
+    def __call__(self, glyphs):
         """ Look up the trigram matrix to get probability of the a sequence of
             one, two or three given glyphs
             TODO : Do better smoothing based on unigram and bigram frequencies
         """
-        if self.tricount is None:
+        if self.tri is None:
             raise ValueError("Trigram not set")
 
-        if len(chars) < 2:
+        if len(glyphs) == 0:
             return 0
 
-        a = chars[-3] if len(chars) > 2 else ' '
-        try:
-            val = self.tricount[a][chars[-2]][chars[-1]]
-            # print('Found: {} : {}'.format(''.join(chars[-3:]), val))
-        except KeyError:
-            val = 0
-            # print("Not found {}".format(''.join(chars[-3:])))
+        elif len(glyphs) == 1:
+            try:
+                num, denom = self.bi[" "][glyphs[0]], self.uni[" "]
+                print('Y|{}| : {}/{}'.format('|'.join(glyphs).replace(" ", "_"), num, denom))
+            except KeyError:
+                num, denom = 0, 1
+                print("X|{}|".format('|'.join(glyphs).replace(" ", "_")))
 
-        return np.log(1e-6 + val)
+        else:
+            a = glyphs[-3] if len(glyphs) > 2 else ' '
+            try:
+                num, denom = self.tri[a][glyphs[-2]][glyphs[-1]], self.bi[glyphs[-2]][glyphs[-1]]
+                print('Y|{}| : {}/{}'.format('|'.join(glyphs[-3:]).replace(" ", "_"), num, denom))
+
+            except KeyError:
+                num, denom = 0, 1
+                print("X|{}|".format('|'.join(glyphs[-3:]).replace(" ", "_")))
+
+        return np.log(1e-6 + num/denom)
 
 priorer = _Prior()
 
 
 class Path():
     """ Class implements one instance of a probable path
-        chars       : the characters/glyphs making up the path
+        glyphs      : the glyphs making up the path
         posterior   : log posterior probability (sum of liklihood and gramprior)
         liklihood   : log liklihood probability
         gramprior   : log prior probability
@@ -57,13 +67,13 @@ class Path():
 
     def __init__(self, arg=None):
         if arg is None:
-            self.chars = []
+            self.glyphs = []
             self.posterior = 0
             self.liklihood = 0
             self.gramprior = 0
 
         elif isinstance(arg, Path):
-            self.chars = arg.chars[:]
+            self.glyphs = arg.glyphs[:]
             self.posterior = arg.posterior
             self.liklihood = arg.liklihood
             self.gramprior = arg.gramprior
@@ -72,30 +82,30 @@ class Path():
             raise TypeError("Got invalid arg {} of type {}".format(
                 arg, type(arg)))
 
-    def add_next_char(self, char, lik=0):
-        self.chars.append(char)
+    def add_next_glyph(self, glyph, lik=0):
+        self.glyphs.append(glyph)
         self.update_posterior(lik)
 
     def update_posterior(self, lik):
         self.liklihood += lik
-        self.gramprior += priorer(self.chars)
+        self.gramprior += priorer(self.glyphs)
         self.posterior = self.liklihood + self.gramprior
 
-    def beget(self, char, lik):
+    def beget(self, glyph, lik):
         child = Path(self)
-        child.add_next_char(char, lik)
+        child.add_next_glyph(glyph, lik)
         return child
 
     def __str__(self):
-        return '{} : LIK{:3.2f} + PRI{:3.2f} = PST{:3.2f}'.format(
-            ''.join(self.chars),
+        return '{} : L{:.2f}+R{:.2f}=T{:.2f}'.format(
+            ''.join(self.glyphs),
             self.liklihood,
             self.gramprior,
             self.posterior,
         )
 
     def text(self):
-        return ''.join(self.chars)
+        return ''.join(self.glyphs)
 
 
 class Paths():
@@ -104,23 +114,35 @@ class Paths():
         paths: the actual ocr_paths
     """
 
-    def __init__(self, order):
-        self.order = order
+    def __init__(self):
         self.paths = [Path()]
 
     def update(self, liklies):
-        """ Given the set of probable characters for the next glyphs,
+        """ Given the set of probable candidates for the next glyph,
             creates more child paths and keeps only the best ones.
         """
-        tmp_paths = [path.beget(ch, lk) for path in self.paths
-                     for ch, lk in liklies]
-        tmp_paths = sorted(tmp_paths, key=lambda p: p.posterior, reverse=True)
-        self.paths = tmp_paths[:self.order]
+        tmp_paths, n = [], 0
+        for glp, lik in liklies:
+            n += 1
+            best = -np.inf
+            for path in self.paths:
+                child = path.beget(glp, lik)
+                if child.posterior > best:
+                    best = child.posterior
+                    bestchild = child
+            tmp_paths.append(bestchild)
+            #print("{} {:.1e} {}".format(glp, lik, bestchild))
 
-    def simple_update(self, char, sort=False):
-        """ Just adds the same character to all the paths (usually a space)"""
+        self.paths = tmp_paths
+        print("Recieved {} likelies, created {} paths".format(
+            n, len(self.paths)))
+        for p in sorted(self.paths, key=lambda x:x.posterior, reverse=True):
+            print(p)
+
+    def simple_update(self, glyph, sort=False):
+        """ Just adds the same glyph to all the paths (usually a space)"""
         for path in self.paths:
-            path.add_next_char(char)
+            path.add_next_glyph(glyph)
         if sort:
             self.paths = sorted(self.paths, key=lambda p: p.posterior,
                                 reverse=True)