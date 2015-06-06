Not scaling aux data; minor bug in ngram lookup; colourful html.

classify_banti.py
@@ -81,7 +81,7 @@ for nsamples, metas, data in gg():
         line, word, aux0, aux1 = meta
 
         if ntwk.takes_aux():
-            aux_data = np.array([[(aux0/ht, aux1/ht), (aux0/ht, aux1/ht)]], dtype='float32')
+            aux_data = np.array([[(aux0, aux1), (aux0, aux1)]], dtype='float32')
             logprobs_or_feats, preds = tester(img, aux_data)
         else:
             logprobs_or_feats, preds = tester(img)

classify_training_data.py
@@ -31,7 +31,7 @@ import theano
 from theanet.neuralnet import NeuralNet
 from iast_unicodes import get_index_to_char_converter
 
-############################################## Helpers
+# ############################################# Helpers
 
 
 def read_json_bz2(path2data):
@@ -46,7 +46,7 @@ def share(data, dtype=theano.config.floatX):
     return theano.shared(np.asarray(data, dtype), borrow=True)
 
 
-############################################## Arguments
+# ############################################# Arguments
 print(' '.join(sys.argv))
 if len(sys.argv) < 4:
     print('''Usage:
@@ -62,7 +62,7 @@ aux_data_file = sys.argv[1] + '.lines.bz2'
 codes_file = sys.argv[2]
 neural_net_files = sys.argv[3:]
 
-############################################## Load Codes
+# ############################################# Load Codes
 with open(codes_file, 'r') as codes_fp:
     codes = ast.literal_eval(codes_fp.read())
 index_to_char = get_index_to_char_converter(codes)
@@ -126,15 +126,16 @@ def test_on_network(neural_net_file):
                 errors[index_to_char(truth)][index_to_char(guess)].append(
                     (index, rank_of_truth, prob_of_truth, prob_of_first))
 
-        if ibatch % 200 == 0 or ibatch == n_batches-1:
+        if ibatch % 200 == 0 or ibatch == n_batches - 1:
             print('{} of {} batches. Errors Train:{} Test:{}'.format(
                 ibatch + 1, n_batches, *train_test_errs))
             #if i == 100: break
 
-    cum_err_rate = sum(wrongs.values())/sum(counts.values())
+    cum_err_rate = sum(wrongs.values()) / sum(counts.values())
     train_test_errs[0] /= n_training_batches * batch_sz
-    train_test_errs[1] /= (n_batches-n_training_batches) * batch_sz
-    print("Error Rates Cum:{:.2%} Train:{:.2%} Test:{:.2%}".format(cum_err_rate, *train_test_errs))
+    train_test_errs[1] /= (n_batches - n_training_batches) * batch_sz
+    print("Error Rates Cum:{:.2%} Train:{:.2%} Test:{:.2%}".format(
+        cum_err_rate, *train_test_errs))
 
     ####################### Speed Check
     n_time_bathces = 100
@@ -143,38 +144,50 @@ def test_on_network(neural_net_file):
     for ibatch in range(n_time_bathces):
         tester(ibatch)
     elapsed = time.time() - start
-    avg_time = 1000*elapsed/(n_time_bathces*batch_sz)
+    avg_time = 1000 * elapsed / (n_time_bathces * batch_sz)
     print("{:.3f}ms per glyph on {}".format(avg_time, theano.config.device))
 
-    return {"ntwk":ntwk,
-            "cum_err_rate":cum_err_rate,
-            "train_test_errs":train_test_errs,
-            "avg_time":avg_time,
+    return {"ntwk": ntwk,
+            "cum_err_rate": cum_err_rate,
+            "train_test_errs": train_test_errs,
+            "avg_time": avg_time,
             "wrongs": wrongs,
             "counts": counts,
-            "errors": errors,
-            }
+            "errors": errors, }
 
-####################### HTML
+############################################# HTML
 
 
-def html(neural_net_file,  ntwk,
+def html(neural_net_file, ntwk,
          cum_err_rate, train_test_errs, avg_time,
          wrongs, counts, errors):
-
-    head = '''<!DOCTYPE html>
-    <html><head><meta charset="UTF-8"></head>
-    <body><h2>Errors for file {0} using the parameters {1}</h2>
-    <h2>Cumulative error rate: {3:.2%}</h2>
-    </br>Training: {4[0]:.2%} Test: {4[1]:.2%}
-    <h3>Average time per glyph: {5:.3f}ms on {6}</h2>
-    <h2>Network</h2> <pre>{2}</pre>
-    </br>N) Character_Shown:(wrongs of tested = error%)
-    </br> Character_Seen Image_Shown(rank, truth's prob% vs max's prob%)...
-    '''.format(x_data_file,
-               neural_net_file, ntwk,
-               cum_err_rate, train_test_errs,
-               avg_time, theano.config.device)
+    head = '''<!DOCTYPE html><html><head><meta charset="UTF-8"></head><body>
+<h2>Banti Neural Network Errors</h2>
+Dataset: <font face="monospace" color="blue">{0}</font></br>
+Neural Net: <font face="monospace" color="blue">{1}</font></br></br>
+<h4>Error Rates</h4>
+<font face="monospace" color="brown">{2:.2%}</font> cumulative</br>
+<font face="monospace" color="brown">{3[0]:.2%}</font> training</br>
+<font face="monospace" color="brown">{3[1]:.2%}</font> test</br>
+<h4>Speed</h4>
+<font face="monospace" color="green">{4:.3f}</font>ms per glyph on
+<font face="monospace" color="green">{5}</font>
+<h4>Network</h4>
+<h5>Specified Parameters:</h5><pre>{6}</pre>
+<h5>Training Parameters:</h5><pre>{7}</pre>
+<h5>Weights:</h5><pre>{8}</pre>
+<h5>Generated Network:</h5><pre>{9}</pre>
+<h4>Legend</h4><pre>sl#) [true_class]: ([wrongs] of [tested] = [error]%)</br>
+[false] [image_shown]([rank], [true_probability]% vs [false_probability]%)...
+</pre>
+<h4>Results</h4>
+'''.format(x_data_file, neural_net_file,
+           cum_err_rate, train_test_errs,
+           avg_time, theano.config.device,
+           ntwk.get_layers_info(),
+           ntwk.get_training_params_info(),
+           ntwk.get_wts_info(detailed=True),
+           ntwk)
 
     filler_main = '\n</br><p> {}) {} ({} of {} = {:.2f}%)</p>'
     filler_sub = '\n</br>{}'
@@ -182,26 +195,26 @@ def html(neural_net_file,  ntwk,
                  '({3}, {4} vs. {5} )'
     tail = '\n</body></html>'
 
-
     ####################### Write HTML
     out_file_name = x_data_file.replace('.bz2', '.with.') + \
                     os.path.basename(neural_net_file.replace('.pkl', '.html'))
     print('Compiling output html file ', out_file_name, end="\n\n")
-    out_file = open(out_file_name, 'w',)
+    out_file = open(out_file_name, 'w', )
     out_file.write(head)
 
     for ibatch in range(n_classes):
         # Write Summary
         u = index_to_char(ibatch)
         error_rate = 100.0 * wrongs[u] / counts[u] if counts[u] > 0 else 0.
-        out_file.write(filler_main.format(ibatch, u, wrongs[u], counts[u], error_rate))
+        out_file.write(
+            filler_main.format(ibatch, u, wrongs[u], counts[u], error_rate))
 
         # Write each bad classification
         for k, v in errors[u].items():
             out_file.write(filler_sub.format(k))
 
             for entry, rank_of_truth, prob_truth, prob_max in v:
-                img = Image.fromarray(255 * (1-x_data_int[entry]))
+                img = Image.fromarray(255 * (1 - x_data_int[entry]))
                 buf = io.BytesIO()
                 img.save(buf, format='BMP')
                 im64 = base64.b64encode(buf.getvalue())
@@ -211,7 +224,7 @@ def html(neural_net_file,  ntwk,
                                                  meta_data[entry][1],
                                                  rank_of_truth,
                                                  prob_max,
-                                                 prob_truth,))
+                                                 prob_truth, ))
 
     out_file.write(tail)
     out_file.close()
@@ -221,5 +234,7 @@ for nnfile in neural_net_files:
     try:
         ret = test_on_network(nnfile)
         html(nnfile, **ret)
+    except (KeyboardInterrupt, SystemExit):
+        raise
     except:
-        pass
+        print("Unexpected error:", sys.exc_info()[0])

ngram/path.py
@@ -37,7 +37,7 @@ class _Prior():
             return 0
 
         elif len(glyphs) == 1:
-            lookup = '|'.join(glyphs).replace(" ", "_")
+            lookup = ('_|'+ glyphs[0])
             try:
                 num, denom = self.bi[" "][glyphs[0]], self.uni[" "]
                 logging.info('Y|{}| : {}/{}'.format(lookup, num, denom))
@@ -50,14 +50,17 @@ class _Prior():
             b, c = glyphs[-2:]
             lookup = '|'.join((a, b, c)).replace(" ", "_")
             try:
-                num, denom = self.tri[a][b][c], self.bi[b][c]
+                num, denom = self.tri[a][b][c], self.bi[a][b]
                 logging.info('Y|{}| : {}/{}'.format(lookup, num, denom))
 
             except KeyError:
                 num, denom = 0, 1
                 logging.info("X|{}|".format(lookup))
 
-        return np.log(1e-6 + num/denom)
+        if num == 0:
+            return -12
+        else:
+            return np.log(num/denom)
 
 priorer = _Prior()
 