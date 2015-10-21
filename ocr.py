60e2ca5c475bd8c466c6a86d85a399a1606b46ae
rakeshvar <rakeshvar@gmail.com>
2015-10-20 17:29:21 -0700

    Made the OCRing function a class. Now the new executable is recognize.py

--- ocr.py
+++ ocr.py
@@ -1,5 +1,4 @@
 import logging
-import sys
 
 from ngramgraph import GramGraph
 from post_process import post_process
@@ -9,84 +8,67 @@ from classifier import Classifier
 from ngram import Ngram
 
 
-def argue(num, default):
-    try:
-        arg = sys.argv[num]
-        assert arg[-4:] == default[-4:]
-    except (IndexError, AssertionError):
-        arg = default
-    return arg
+class OCR():
+    def __init__(self,
+                 nnet_fname,
+                 scaler_fname,
+                 labels_fname,
+                 ngram_fname,
+                 logbase=1,
+                 loglevel=logging.INFO,):
+        self.nnet_fname = nnet_fname
+        self.scaler_fname = scaler_fname
+        self.labels_fname = labels_fname
+        self.ngram_fname = ngram_fname
+        self.logbase = logbase
+        self.loglevel = loglevel
+        self.loglevelname = logging._levelToName[loglevel].lower()
 
-############################## Read Arguments
-banti_fname = "sample_images/praasa.box"
-nnet_fname = "library/nn.pkl"
-scaler_fname = "scalings/relative48.scl"
-labels_fname = "labellings/alphacodes.lbl"
-ngram_fname = "library/mega.123.pkl"
+        Bantry.scaler = ScalerFactory(scaler_fname)
+        Bantry.classifier = Classifier(nnet_fname, labels_fname,
+                                       logbase=logbase)
+        self.ng = Ngram(ngram_fname)
+        Bantry.ngram = self.ng
+        GramGraph.set_ngram(self.ng)
 
-if len(sys.argv) < 2:
-    print("Usage:\n"
-          "{} [box={}] [nnet={}] [scaler={}] [labels={}] [ngram={}] -[log=info]"
-          "\nloglevel -d:debug, -i:info, -w:warn, -e:error, -c:critical"
-          "\nContinuing with defaults"
-          "".format(sys.argv[0], banti_fname, nnet_fname,
-                    scaler_fname, labels_fname, ngram_fname))
+    def ocr_box_file(self, box_fname):
+        # Set up the names of output files
+        replace = lambda s: box_fname.replace('.box', s)
 
-banti_fname = argue(1, banti_fname)
-nnet_fname = argue(2, nnet_fname)
-scaler_fname = argue(3, scaler_fname)
-labels_fname = argue(4, labels_fname)
-ngram_fname = argue(5, ngram_fname)
+        asis_fname = replace('.ml.txt')
+        nogram_out_fname = replace('.nogram.txt')
+        ngram_out_fname = replace('.gram.txt')
 
-loglevel = {'-c': logging.CRITICAL,
-            '-e': logging.ERROR,
-            '-w': logging.WARNING,
-            '-i': logging.INFO,
-            '-d': logging.DEBUG}.get(sys.argv[-1][:2].lower(),
-                                     logging.INFO)
+        log_fname = replace('.{}.log'.format(self.loglevelname))
+        logging.basicConfig(filename=log_fname,
+                            level=self.loglevel,
+                            filemode="w")
 
-replace = lambda s: banti_fname.replace('.box', s)
-log_fname = replace('.{}.log'.format(logging._levelToName[loglevel]).lower())
-asis_fname = replace('.ml.txt')
-nogram_out_fname = replace('.nogram.txt')
-ngram_out_fname = replace('.gram.txt')
+        # Read Bantries & get Most likely output
+        bf = BantryFile(box_fname)
+        with open(asis_fname, 'w', encoding='utf-8') as f:
+            f.write(post_process(bf.text))
 
-logging.basicConfig(filename= log_fname,
-                    level=loglevel, filemode="w")
+        # Process using ngrams
+        ngrammed_lines, notgrammed_lines = [], []
+        for linenum in range(bf.num_lines):
+            print("Line ", linenum)
+            line_bantries = bf.get_line_bantires(linenum)
+            gramgraph = GramGraph(line_bantries)
+            gramgraph.process_tree()
+            notgrammed_lines.append(gramgraph.get_best_apriori_str())
+            ngrammed_lines.append(gramgraph.get_best_str())
 
-############################## Set-up scaler, classifier, ngram etc.
-Bantry.scaler = ScalerFactory(scaler_fname)
-Bantry.classifier = Classifier(nnet_fname, labels_fname, logbase=1)
-ng = Ngram(ngram_fname)
-Bantry.ngram = ng
-GramGraph.set_ngram(ng)
+        nogram_out = post_process("\n".join(notgrammed_lines))
+        with open(nogram_out_fname, 'w', encoding='utf-8') as out_file:
+            out_file.write(nogram_out)
 
-############################## Read Bantries & get Most likely output
-bf = BantryFile(banti_fname)
-with open(asis_fname, 'w', encoding='utf-8') as f:
-    f.write(post_process(bf.text))
+        ngram_out = post_process("\n".join(ngrammed_lines))
+        with open(ngram_out_fname, 'w', encoding='utf-8') as out_file:
+            out_file.write(ngram_out)
 
-############################## Process using ngrams
-ngrammed_lines, notgrammed_lines = [], []
-
-for linenum in range(bf.num_lines):
-    print("Line ", linenum)
-    line_bantries = bf.get_line_bantires(linenum)
-    gramgraph = GramGraph(line_bantries)
-    gramgraph.process_tree()
-    notgrammed_lines.append(gramgraph.get_best_apriori_str())
-    ngrammed_lines.append(gramgraph.get_best_str())
-
-nogram_out = post_process("\n".join(notgrammed_lines))
-with open(nogram_out_fname, 'w', encoding='utf-8') as out_file:
-    out_file.write(nogram_out)
-
-ngram_out = post_process("\n".join(ngrammed_lines))
-with open(ngram_out_fname, 'w', encoding='utf-8') as out_file:
-    out_file.write(ngram_out)
-
-print("Input : ", banti_fname)
-print("As is output : ", asis_fname)
-print("Without ngram : ", nogram_out_fname)
-print("With ngram : ", ngram_out_fname)
-print("Log : ", log_fname)
+        print("Input : ", box_fname)
+        print("As is output : ", asis_fname)
+        print("Without ngram : ", nogram_out_fname)
+        print("With ngram : ", ngram_out_fname)
+        print("Log : ", log_fname)
---
+++ recognize.py
@@ -0,0 +1,211 @@
+#!/usr/bin/env python3
+
+############################### Parse Input Arguments #########################
+import argparse
+import logging
+
+
+class Formatter(argparse.RawDescriptionHelpFormatter,
+                argparse.ArgumentDefaultsHelpFormatter):
+    pass
+
+desc = '''Telugu OCR
+    Performs OCR on a given tif, box or pdf file. Or a directory with files.
+    Tiff file should be 1bpp (i.e. encoded in binary).
+    box file is the output of banti_segmenter.
+    pdf will be converted to tiff files.
+    examples:
+         python3 {0} ~/books/andhra_maha.pdf
+         python3 {0} ~/books/andhra_maha1.tif
+         python3 {0} ~/books/andhra_maha1.box
+         python3 {0} ~/books/andhra_maha/'''.format(__file__)
+
+prsr = argparse.ArgumentParser(description=desc, formatter_class=Formatter)
+
+prsr.add_argument('-n', action='store', dest='nnet_fname',
+                  default='library/nn.pkl',
+                  help='The file where the neural network parmeters are stored')
+prsr.add_argument('-s', action='store', dest='scaler_fname',
+                  default='scalings/relative48.scl',
+                  help='The scaling that should be applied to each image')
+prsr.add_argument('-l', action='store', dest='labels_fname',
+                  default='labellings/alphacodes.lbl',
+                  help='The labels for each Telugu glyph')
+prsr.add_argument('-g', action='store', dest='ngram_fname',
+                  default='library/mega.123.pkl',
+                  help='The nGram dictionaries')
+prsr.add_argument('-b', action='store', dest='banti_segmenter',
+                  default='./banti_segmenter',
+                  help='The binary that convers tiff files to box files.')
+prsr.add_argument('--calib', action='store', dest='calibration', type=float,
+                  default=1,
+                  help='The correction that needs to be applied to '
+                       'the nnet\'s outputs')
+prsr.add_argument('--log', action='store', dest='log_level',
+                  default='info',
+                  help='Level of logging: debug, info, critical etc.')
+prsr.add_argument('input_file_or_dir', action='store',
+                  help='Can be pdf, tiff, or box file. Or a directory full of '
+                       'tiff or box files.')
+
+args = prsr.parse_args()
+
+args.log_level = {
+    'critical': logging.CRITICAL,
+    'error': logging.ERROR,
+    'warning': logging.WARNING,
+    'info': logging.INFO,
+    'debug': logging.DEBUG}.get(args.log_level.lower(), logging.INFO)
+
+print('Command line Arguments')
+for k in sorted(vars(args)):
+    print('\t{:20}{}'.format(k, vars(args)[k]))
+print()
+
+
+############################# Helper Functions ################################
+import os
+import subprocess
+
+
+def change_ext(fname, ext):
+    name, _ = os.path.splitext(fname)
+    if ext[0] != '.':
+        ext = '.' + ext
+
+    return name + ext
+
+
+def is_file_of_type(fname, ext):
+    if ext == 'tif':
+        checks = '.tif', '.tiff', '.TIF', '.TIFF'
+
+    elif ext == 'box':
+        checks = '.box', '.BOX'
+
+    elif ext == 'pdf':
+        checks = '.pdf', '.PDF'
+
+    elif ext == 'dir':
+        return os.path.isdir(fname)
+
+    else:
+        raise ValueError('Unknown extension', ext)
+
+    for e in checks:
+        if fname.endswith(e):
+            return True
+    return False
+
+
+def run_command(command, timeout=0, prinout=True):
+    print('Launched command with timeout={}'
+          '\n"{}"'.format(timeout, ' '.join(command)))
+    proc = subprocess.Popen(command,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+    success = True
+
+    if timeout:
+        try:
+            outs, errs = proc.communicate(timeout=timeout)
+        except subprocess.TimeoutExpired:
+            proc.kill()
+            outs, errs = proc.communicate()
+            success = False
+
+    else:
+        outs, errs = proc.communicate()
+
+    if prinout:
+        if success:
+            print('Success')
+            print('STDOUT:\n', outs.decode('utf-8'))
+            print('STDERR:\n', errs.decode('utf-8'))
+        else:
+            print('Failure')
+            print('STDOUT:\n', outs.decode('utf-8')[-100:])
+            print('STDERR:\n', errs.decode('utf-8')[-100:])
+
+    return success, outs, errs
+
+
+def pdf_to_tiffs(infile):
+    img_dir = os.path.splitext(os.path.abspath(infile))[0]
+    if not os.path.exists(img_dir):
+        os.makedirs(img_dir)
+
+    img_namer = os.path.join(img_dir, 'page_%04d.tif')
+
+    gscommand = ['gs', '-dNOPAUSE',
+                 '-q', '-r400', '-sDEVICE=tiffg4', '-dBATCH',
+                 #'-dFirstPage=5', '-dLastPage=6',
+                 '-dGrayValues=2',
+                 '-sOutputFile=' + img_namer,
+                 infile]
+
+    run_command(gscommand)
+    return img_dir
+
+
+def tiff_to_box(banti_segmenter, f):
+    succ, _, _ = run_command([banti_segmenter, f], timeout=10)
+
+    if succ:
+        return change_ext(f, '.box')
+
+
+def tiff_dir_to_box(img_dir, banti_segmenter):
+    print('Converting tiff images in', img_dir, 'to box files.')
+    for f in sorted(os.listdir(img_dir)):
+        if not is_file_of_type(f, 'tif'):
+            continue
+        f = os.path.join(img_dir, f)
+        if not os.path.exists(change_ext(f, '.box')):
+            tiff_to_box(banti_segmenter, f)
+
+
+# ###################################### Load OCR
+from ocr import OCR
+
+print('Initializing the OCR')
+recognizer = OCR(args.nnet_fname,
+                 args.scaler_fname,
+                 args.labels_fname,
+                 args.ngram_fname,
+                 args.calibration,
+                 args.log_level)
+print('Done')
+
+
+# ###################################### Helpers
+
+def ocr_box_dir(img_dir):
+    print('Recognizing box files in ', img_dir)
+    for f in sorted(os.listdir(img_dir)):
+        if is_file_of_type(f, 'box'):
+            f = os.path.join(img_dir, f)
+            print('OCRing', f)
+            recognizer.ocr_box_file(f)
+
+
+def ocr_dir(img_dir):
+    tiff_dir_to_box(img_dir, args.banti_segmenter)
+    ocr_box_dir(img_dir)
+
+# ###################################### Actual Code
+
+if is_file_of_type(args.input_file_or_dir, 'box'):
+    recognizer.ocr_box_file(args.input_file_or_dir)
+
+elif is_file_of_type(args.input_file_or_dir, 'tif'):
+    box_fname = tiff_to_box(args.banti_segmenter, args.input_file_or_dir)
+    if box_fname:
+        recognizer.ocr_box_file(box_fname)
+
+elif is_file_of_type(args.input_file_or_dir, 'pdf'):
+    imgs_dir = pdf_to_tiffs(args.input_file_or_dir)
+    ocr_dir(imgs_dir)
+
+elif is_file_of_type(args.input_file_or_dir, 'dir'):
+    ocr_dir(args.input_file_or_dir)
